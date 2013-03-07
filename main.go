903ee59b0ce9a99d78c63849da888dbed01c6547
Kamil Kisiel <kamil@kamilkisiel.net>
2013-03-06 22:21:19 -0800

    Move package finding to its own function. Fixes #10.

--- main.go
+++ main.go
@@ -101,22 +101,10 @@ func main() {
 		flag.Usage()
 		Fatal("you must specify a package")
 	}
-
-	var err1, err2 error
-
-	// First try to treat pkgName as import path...
-	pkg, err1 := importPathToPkg(pkgName)
-	if err1 != nil {
-		// ... then attempt as file path
-		pkg, err2 = directoryToPkg(pkgName)
+	pkg, err := findPackage(pkgName)
+	if err != nil {
+		Fatal("%s", err)
 	}
-
-	if err2 != nil {
-		// Print both errors so the user can see in what ways the
-		// package lookup failed.
-		Fatal("could not import %s: %s\n%s", pkgName, err1, err2)
-	}
-
 	files := getFiles(pkg)
 
 	if err := checkFiles(files, ignore.re, ignorePkg.items); err != nil {
@@ -127,20 +115,31 @@ func main() {
 	}
 }
 
-// directoryToPkg returns a Package found in a directory
-func directoryToPkg(directory string) (*build.Package, error) {
-	ctx := build.Default
-	ctx.CgoEnabled = false
-	pkg, err := ctx.ImportDir(directory, 0)
-	return pkg, err
-}
+// findPackage finds a package.
+// path is first tried as an import path and if the package is not found, as a filesystem path.
+func findPackage(path string) (*build.Package, error) {
+	var (
+		err1, err2 error
+		pkg        *build.Package
+	)
 
-// importPathToPkg returns a Package found at a package path
-func importPathToPkg(path string) (*build.Package, error) {
 	ctx := build.Default
 	ctx.CgoEnabled = false
-	pkg, err := ctx.Import(path, ".", 0)
-	return pkg, err
+
+	// First try to treat path as import path...
+	pkg, err1 = ctx.Import(path, ".", 0)
+	if err1 != nil {
+		// ... then attempt as file path
+		pkg, err2 = ctx.ImportDir(path, 0)
+	}
+
+	if err2 != nil {
+		// Print both errors so the user can see in what ways the
+		// package lookup failed.
+		return nil, fmt.Errorf("could not import %s: %s\n%s", path, err1, err2)
+	}
+
+	return pkg, nil
 }
 
 // getFiles returns all the Go files found in a package
