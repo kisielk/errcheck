c81f13613ace5632feec6fd9ad39e31cb9e573aa
Dominik Honnef <dominikh@fork-bomb.org>
2013-10-16 18:28:27 +0200

    update to new importer API

--- lib/errcheck.go
+++ lib/errcheck.go
@@ -98,7 +98,7 @@ func typeCheck(p package_) (typedPackage, error) {
 		Types:   tp.callTypes,
 		Objects: tp.identObjs,
 	}
-	context := types.Config{Import: importer.NewImporter().Import}
+	context := types.Config{Import: importer.New().Import}
 
 	_, err := context.Check(p.path, p.fset, p.astFiles, &info)
 	return tp, err
