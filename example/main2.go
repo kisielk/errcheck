1167d3d6ae6514f3c863c45a8c196378221ec0b5
Ben Johnson <benbjohnson@yahoo.com>
2014-09-09 10:23:59 -0600

    Add line pragma test case.

--- example/main2.go
+++ example/main2.go
@@ -7,6 +7,8 @@ type t struct{}
 
 func (x t) a() error {
 	fmt.Println("this method returns an error") // UNCHECKED
+//line myfile.txt:100
+	fmt.Println("this method also returns an error") // UNCHECKED
 	return nil
 }
 
