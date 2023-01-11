0ac6860123db877fc79ea5101e1f53c73b6d09ae
Gabriel Burt <gburt@devoted.com>
2023-01-11 17:05:39 +0000

    test check blanks, including a failing test for `var` declarations

--- errcheck/analyzer_test.go
+++ errcheck/analyzer_test.go
@@ -1,12 +1,29 @@
 package errcheck
 
 import (
-	"golang.org/x/tools/go/analysis/analysistest"
 	"path/filepath"
 	"testing"
+
+	"golang.org/x/tools/go/analysis/analysistest"
 )
 
 func TestAnalyzer(t *testing.T) {
-	packageDir := filepath.Join(analysistest.TestData(), "src/a/")
-	_ = analysistest.Run(t, packageDir, Analyzer)
+	t.Run("default flags", func(t *testing.T) {
+		packageDir := filepath.Join(analysistest.TestData(), "src/a/")
+		_ = analysistest.Run(t, packageDir, Analyzer)
+	})
+
+	t.Run("check blanks", func(t *testing.T) {
+		packageDir := filepath.Join(analysistest.TestData(), "src/blank/")
+		Analyzer.Flags.Set("blank", "true")
+		_ = analysistest.Run(t, packageDir, Analyzer)
+		Analyzer.Flags.Set("blank", "false") // reset it
+	})
+
+	t.Run("check asserts", func(t *testing.T) {
+		packageDir := filepath.Join(analysistest.TestData(), "src/assert/")
+		Analyzer.Flags.Set("assert", "true")
+		_ = analysistest.Run(t, packageDir, Analyzer)
+		Analyzer.Flags.Set("assert", "false") // reset it
+	})
 }
--- errcheck/testdata/src/blank/main.go
+++ errcheck/testdata/src/blank/main.go
@@ -1,9 +1,37 @@
 package blank
 
+import "fmt"
+
 func a() error {
 	return nil
 }
 
+func b() (string, error) {
+	return "", nil
+}
+
+func c() string {
+	return ""
+}
+
 func main() {
 	_ = a() // want "unchecked error"
+	a()     // want "unchecked error"
+	b()     // want "unchecked error"
+	c()     // ignored, doesn't return an error
+
+	{
+		r, err := b() // fine, we're checking the error
+		fmt.Printf("r = %v, err = %v\n", r, err)
+	}
+
+	{
+		r, _ := b() // want "unchecked error"
+		fmt.Printf("r = %v\n", r)
+	}
+
+	{
+		var r, _ = b() // want "unchecked error"
+		fmt.Printf("r = %v\n", r)
+	}
 }
